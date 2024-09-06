8d422b52ff927d2d26029c1d9d88f779f5a625f7
saifulaija <saifulaija2023@gmail.com>
2024-09-07 02:32:00 +0600

    footer redesign done

--- furniflex-client/src/assets/index.ts
+++ furniflex-client/src/assets/index.ts
@@ -5,6 +5,7 @@ import register from "./images/register.png";
 import logoText from "./images/logo-text.png";
 import user from "./images/user.png";
 import nodata from "./images/no-data.png";
+import footer_logo from "./images/footer-logo.png";
 
 const assets = {
   images: {
@@ -12,7 +13,8 @@ const assets = {
     logoText,
     user,
     register,
-    nodata
+    nodata,
+    footer_logo
   },
 };
 
--- furniflex-client/src/components/shared/Footer/Footer.tsx
+++ furniflex-client/src/components/shared/Footer/Footer.tsx
@@ -1,43 +1,42 @@
-
-
 import assets from "@/assets";
-
+import { Separator } from "@/components/ui/separator";
 
 const Footer = () => {
   const resources = [
-    { title: "Blog", link: "/blog" },
-    { title: "Help Center", link: "/help-center" },
-    { title: "Release Notes", link: "/release-notes" },
-    { title: "Status", link: "/status" },
+    { title: "Master Plan", link: "/blog" },
+    { title: "Jobs", link: "/help-center" },
+    { title: "Invest", link: "/release-notes" },
+    { title: "Pressroom", link: "/status" },
+    { title: "Blog", link: "/status" },
+    { title: "Contact", link: "/status" },
   ];
 
   const community = [
-    { title: "Twitter", link: "https://twitter.com" },
-    { title: "LinkedIn", link: "https://linkedin.com" },
-    { title: "Facebook", link: "https://facebook.com" },
-    { title: "Dribbble", link: "https://dribbble.com" },
-    { title: "Podcast", link: "/podcast" },
+    { title: "Unlock my Robot Power", link: "https://twitter.com" },
+    { title: "Starlight", link: "https://linkedin.com" },
+    { title: "Robot Platform", link: "https://facebook.com" },
+    { title: "EEVE Roadmap", link: "https://dribbble.com" },
+  
   ];
 
   const company = [
-    { title: "About Us", link: "/about-us" },
-    { title: "Careers", link: "/careers" },
-    { title: "Legal", link: "/legal" },
+    { title: "Williow X Community", link: "/about-us" },
+    { title: "Developer & Maker Access", link: "/careers" },
+    { title: "Special Cases", link: "/legal" },
   ];
 
   return (
-    <footer className="bg-primary text-white py-8 mt-12">
+    <footer className="bg-black text-white py-8 mt-12">
       <div className="container mx-auto px-4 md:px-8">
         <div className="flex flex-wrap justify-between">
           {/* Abstract Section */}
           <div className="w-full sm:w-1/5 mb-6">
-            <h5 className="text-xl font-bold mb-4">Abstract</h5>
-            <p>Branches</p>
+            <img src={assets.images.footer_logo} width={100} height={100} alt="logo" />
           </div>
 
           {/* Resources Section */}
           <div className="w-full sm:w-1/5 mb-6">
-            <h5 className="text-xl font-bold mb-4">Resources</h5>
+            <h5 className="text-xl font-bold mb-4">About Us</h5>
             <ul>
               {resources.map((resource) => (
                 <li key={resource.title}>
@@ -55,7 +54,7 @@ const Footer = () => {
 
           {/* Community Section */}
           <div className="w-full sm:w-1/5 mb-6">
-            <h5 className="text-xl font-bold mb-4">Community</h5>
+            <h5 className="text-xl font-bold mb-4">Explore EEVE</h5>
             <ul>
               {community.map((item) => (
                 <li key={item.title}>
@@ -73,7 +72,7 @@ const Footer = () => {
 
           {/* Company Section */}
           <div className="w-full sm:w-1/5 mb-6">
-            <h5 className="text-xl font-bold mb-4">Company</h5>
+            <h5 className="text-xl font-bold mb-4">Community & Support</h5>
             <ul>
               {company.map((item) => (
                 <li key={item.title}>
@@ -87,22 +86,10 @@ const Footer = () => {
                 </li>
               ))}
             </ul>
-            <div className="mt-4">
-              <p>Contact Us</p>
-              <a href="mailto:info@abstract.com" className="hover:underline">
-                info@abstract.com
-              </a>
-            </div>
-          </div>
-
-          {/* Logo and Copyright Section */}
-          <div className="w-full sm:w-1/5 mb-6 mt-40">
-            <img src={assets.images.logo} width={100} height={100} alt="logo" />
-            <p className="text-sm mb-1">Copyright © 2022</p>
-            <p className="text-sm mb-1">Abstract Studio Design, Inc.</p>
-            <p className="text-sm">All rights reserved</p>
+            
           </div>
         </div>
+        <Separator/>
       </div>
     </footer>
   );
