01697d144d59f176eefd01dc38a7033660bcba8d
saifulaija <saifulaija2023@gmail.com>
2024-09-07 23:05:56 +0600

    final recheck done

--- furniflex-client/src/utils/axiosInstance.ts
+++ furniflex-client/src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: " http://localhost:5000/api/", // Base URL for your backend
+  baseURL: " https://furniflex-server-iota.vercel.app/api/", // Base URL for your backend
   timeout: 10000, // Request timeout
 });
 
